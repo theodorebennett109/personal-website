57723924117a6075c1dd9e8186e1677c15528d2c
tbennett109 <tbennett@realdecoy.com>
2023-06-23 23:44:03 -0500

    navbar created

--- src/components/nav-bar/nav-bar.vue
+++ src/components/nav-bar/nav-bar.vue
@@ -1,7 +1,49 @@
 <style scoped lang="scss" src="./nav-bar.scss"></style>
 <script lang="ts" src="./nav-bar.ts"></script>
+
 <template>
-  <div class="[ nav-bar ]">
-    <h1>nav-bar</h1>
+  <div class="[ w-full h-full ]">
+    <b-navbar type="is-dark">
+      <template #brand>
+        <b-navbar-item
+          class="[ no-underline ]"
+          tag="router-link"
+          :to="{ path: '/' }"
+        >
+          <p class="[ font-bold text-xl font-sans ]">Theodore Bennett</p>
+        </b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/' }"
+          class="[ no-underline ]"
+        >
+          <p>Home</p></b-navbar-item
+        >
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/about' }"
+          class="[ no-underline ]"
+        >
+          <p>About</p></b-navbar-item
+        >
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/portfolio' }"
+          class="[ no-underline ]"
+        >
+          <p>Portfolio</p></b-navbar-item
+        >
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/contact' }"
+          class="[ no-underline ]"
+        >
+          <p>Contact</p></b-navbar-item
+        >
+      </template>
+    </b-navbar>
   </div>
 </template>
